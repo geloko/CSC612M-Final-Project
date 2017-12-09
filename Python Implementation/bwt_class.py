d863a6e54cc92ab3430b41f38adf30fda718df22
Kurt Aquino <kurt_aquino@dlsu.edu.ph>
2017-12-09 20:08:40 +0800

    Optimized Python Implementation + Input Strings

--- Python Implementation/bwt_class.py
+++ Python Implementation/bwt_class.py
@@ -5,47 +5,33 @@ from datetime import timedelta
 class BurrowsWheeler(object):
 
 	def getBWT(self, input):
-		rotations = []
-
 		start_time = datetime.now()
 		print("Started BWT at:", start_time)
 
-		# Step 1 - List all possible rotations of the string
-		for i in range(len(input)):
-		    rotations.append(input[i:] + input[:i])
-
-		# Step 2 - Sort the list of rotations in aphabetical/lexicographical order
-		bwt = sorted(rotations)
+		# Step 1 & 2 - List and sort all of the rotations of the input string in aphabetical/lexicographical order
+		rotations = sorted(range(len(input)), key=lambda i: input[i:]) 
 		    
 		# Step 3 - Get the last characters of the sorted rotations
-		last_column = [row[-1:] for row in bwt]
+		bwt = [input[i - 1] if(i > 0) else input[len(input) - 1] for i in rotations]
 
 		end_time = datetime.now()
 		print("Finished BWT at:", end_time)
 
 		runtime = end_time - start_time
-		print("\nBWT runtime in seconds:", runtime.seconds, ".", runtime.microseconds)
+		print("BWT runtime in seconds:", runtime.seconds, ".", runtime.microseconds)
 
-		print("\nBWT:", "".join(last_column))
+		print("\nBWT:", "".join(bwt))
 
-		for i in range(len(input)):
-		    print(rotations[i])   
-		
-		print('')
-		
-		for i in range(len(input)):
-		    print(bwt[i])
+		return runtime, "".join(bwt)
 
-		return runtime, "".join(last_column)
-
-	def getFMIndex(self, last_column, substring):
+	def getFMIndex(self, bwt, substring):
 		# Perform FM-Index compression
-		search = sorted(last_column)
+		search = sorted(bwt)
 		for i in range(1, len(substring)):
 		    print("\nPass #", i)
 		    search = sorted(search)
-		    for j in range(len(last_column)):
-		        search[j] = last_column[j] + search[j]
+		    for j in range(len(bwt)):
+		        search[j] = bwt[j] + search[j]
 		        
 		    for j in range(len(search)):
 		        print(search[j])
